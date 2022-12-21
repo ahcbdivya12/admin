06f7a62566fc9f88223ca8710821a7903a782394
ahcbdivya12 <121059036+ahcbdivya12@users.noreply.github.com>
2022-12-21 16:38:53 +0530

    Add files via upload

---
+++ partials/sidebar.php
@@ -0,0 +1,59 @@
+       <!-- A vertical navbar -->
+<nav class="navbar bg-light sidebar" >
+<ul class="accordion-container navbar-nav sidebar-list">
+<li class="nav-item">
+      <a class="nav-link content-link" href="dashboard.php"><i class='fas fa-home'></i> Dashboard</a>
+ </li>
+
+<!--==== accordion list-3 =====-->
+<li class="nav-item accordion-content hide">
+<h3 class="title"><i class='fas fa-cog'></i> User</h3>
+<ul class="description navbar-nav">
+
+     <li class="nav-item">
+      <a class="nav-link content-link" href="dashboard.php?cat=website-setting&subcat=website-menu">sing_up data</a>
+    </li>
+
+</ul>
+</li>
+<!--==== accordion list-3 =====-->
+
+<li class="nav-item accordion-content hide">
+<h3 class="title"><i class='fas fa-file-alt'></i> Products</h3>
+<ul class="description navbar-nav">
+  <li class="nav-item">
+      <a class="nav-link content-link" href="dashboard.php?cat=website-content&subcat=jounral">Jounral</a>
+    </li>
+  <li class="nav-item">
+      <a class="nav-link content-link" href="dashboard.php?cat=website-content&subcat=home-content">Books details View & Modify</a>
+    </li>
+     <li class="nav-item">
+      <a class="nav-link content-link" href="dashboard.php?cat=website-content&subcat=music">Musics & modification</a>
+    </li>
+
+     <li class="nav-item">
+      <a class="nav-link content-link" href="dashboard.php?cat=website-content&subcat=therapist">Therapist</a>
+    </li>
+</ul>
+</li>
+<li class="nav-item accordion-content hide">
+<h3 class="title"><i class='fas fa-user-cog'></i> Website Admin</h3>
+<ul class="description navbar-nav">
+  <li class="nav-item">
+      <a class="nav-link content-link" href="dashboard.php?cat=website-admin&subcat=admin-profile">Admin Profile</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link content-link" href="dashboard.php?cat=website-admin&subcat=change-password">Change Password</a>
+    </li>
+</ul>
+</li>
+
+<li class="nav-item text-center">
+<a href="javascript:void(0)" id="hide"><i class='far fa-arrow-alt-circle-left'></i></a>
+</li>
+
+</ul>
+
+
+</nav>
+
